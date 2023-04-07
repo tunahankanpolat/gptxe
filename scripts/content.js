c1c0be45d9611faa34821f90bb2c9fa027a1dd73
tuna <tunahan.kanpolat@ceng.deu.edu.tr>
2023-04-07 17:50:50 +0300

    "operation choose and display response"

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,121 @@
+const iconButtonStyles = {
+  class: 'icon-button',
+  position: 'absolute',
+  imgClass: 'img-icon',
+  height: '100%',
+}
+
+const menuStyles = {
+  class: 'menu',
+  position: 'absolute',
+  height: '100%',
+  background: 'gray'
+}
+
+const menuItemsStyles = {
+  class: 'menu-items',
+}
+
+const menuOptions = [
+  { label: 'Summarize Content', choice: 'summarizeContent' },
+  { label: 'Fix Typos', choice: 'fixTypos' },
+  { label: 'Explain Code', choice: 'explainCode' },
+  { label: 'Check Information', choice: 'checkInformation' },
+];
+
 let isSelectionActive = false;
 document.addEventListener("mouseup", (event) => {
     let selectedText = window.getSelection().toString();
-    const existingButton = document.querySelector('.my-icon-button');
-    if (selectedText.length > 0 && !existingButton) {
+    if (selectedText.length > 0) {
       isSelectionActive = true;
-      showIconButton(event);
+      if(!getIconButton())
+        createIconButton(event);
     }else{
       isSelectionActive = false;
-      existingButton.remove();
-    }
-});
-
-document.addEventListener("selectionchange", () => {
-  if (isSelectionActive) {
-    let selectedText = window.getSelection().toString();
-    if (selectedText.length === 0) {
-      isSelectionActive = false;
       removeIconButton();
+      //removeMenu();
     }
-  }
 });
 
-function showIconButton(event) {
-  // Seçili text'in koordinatlarını almak
+
+function sendMessage(content, choice){
+  console.log("sendMessage run")
+  chrome.runtime.sendMessage({ type: "connectAPI",selectedText: content, operationChoice: choice },(response) =>{
+    removeMenuItems();
+    displayResponse(response.result);
+  console.log(response.result);
+});
+}
+
+function displayResponse(response){
+  let menu = getMenu();
+  console.log(menu);
+  menu.innerHTML = response;
+}
+
+function createMenu(event){
+  console.log("createMenu");
+  const selection = window.getSelection();
+  //console.log(selection.toString());
+  const range = selection.getRangeAt(0);
+  const { left, top, height } = range.getBoundingClientRect();
+  const selectedText = selection.toString();
+  // Mouse pozisyonunu almak
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const menu = document.createElement('div');
+  menu.style.position = menuStyles.position;
+  menu.style.left = `${mouseX}px`;
+  menu.style.top = `${top + height}px`;  
+  menu.style.backgroundColor = menuStyles.background;
+  menu.classList.add('menu');
+  const menuItems = document.createElement('div');
+  menuItems.classList.add('menu-items');
+
+  menu.appendChild(menuItems);
+  menuOptions.forEach((option) => {
+    console.log(option);
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+    menuItem.textContent = option.label;
+
+    menuItem.addEventListener('click', () => {
+      console.log(selectedText);
+      sendMessage(selectedText, option.choice);
+    });
+
+    menuItems.appendChild(menuItem);
+  });
+  document.body.appendChild(menu);
+}
+
+function getMenuItems(){
+  return document.querySelector("."+menuItemsStyles.class);
+}
+
+function removeMenuItems(){
+  let menuItems = getMenuItems();
+  console.log(menuItems);
+  if(menuItems)
+    menuItems.remove();
+  console.log(menuItems);
+}
+
+function getMenu(){
+  return document.querySelector("."+menuStyles.class);
+}
+
+function removeMenu(){
+  let menu = getMenu();
+  if(menu)
+    menu.remove();
+}
+
+
+
+//----------------------Icon Button-------------------------------------
+function createIconButton(event) {
   const selection = window.getSelection();
   const range = selection.getRangeAt(0);
   const { left, top, height } = range.getBoundingClientRect();
@@ -31,53 +124,38 @@ function showIconButton(event) {
   const mouseX = event.clientX;
   const mouseY = event.clientY;
 
-  // Icon butonunu oluşturmak
-  iconButton = document.createElement('button');
-  iconButton.classList.add('my-icon-button');
-  iconButton.innerHTML = '<i class="fas fa-icon"></i>';
-  iconButton.style.position = 'absolute';
+  const iconButton = document.createElement('button');
+  iconButton.classList.add(iconButtonStyles.class);
+  iconButton.style.position = iconButtonStyles.position;
   iconButton.style.left = `${mouseX}px`;
-  iconButton.style.top = `${top + height}px`;
+  iconButton.style.top = `${top + height}px`;  
+
+
+  const iconImg = document.createElement('img');
+  iconImg.classList.add(iconButtonStyles.class);
+  iconButton.appendChild(iconImg);
+
   iconButton.addEventListener('click', () => {
-    iconButton.remove();
-    sendMessage(selection.toString(), "summarizeContent");
+    if (!isSelectionActive) {
+      return;
+    }
+    if (!getMenu()) {
+      createMenu(event);
+      removeIconButton();
+    }
   });
-  // Icon butonunu eklemek
+
   document.body.appendChild(iconButton);
+  console.log(iconButton);
 }
 
-function sendMessage(content, choice){
-  chrome.runtime.sendMessage({ type: "connectAPI",selectedText: content, operationChoice: choice },(response) =>{
-    console.log(response.result);
-  });
+function getIconButton(){
+  return document.querySelector("."+iconButtonStyles.class);
 }
 
-function getIcon(){
-  return document.querySelector('.my-icon-button');
+function removeIconButton(){
+  let iconButton = getIconButton();
+  if(iconButton)
+    iconButton.remove();
 }
 
-function removeIcon(){
-  getIcon().remove();
-}
-
-function createIcon(){
-  iconButton = document.createElement('button');
-  iconButton.classList.add('my-icon-button');
-  iconButton.innerHTML = '<i class="fas fa-icon"></i>';
-  iconButton.style.position = 'absolute';
-  iconButton.style.left = `${mouseX}px`;
-  iconButton.style.top = `${top + height}px`;
-}
-
-/*async function sendMessage(message) {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage(message, (response) => {
-      if (chrome.runtime.lastError) {
-        console.log("sa");
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve(response);
-      }
-    });
-  });
-}*/
