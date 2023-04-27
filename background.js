835d3139f900d5b76b44cb6694bf92ec5cefe86b
tuna <tunahan.kanpolat@ceng.deu.edu.tr>
2023-04-27 18:03:54 +0300

    #8 Changed request type for Rest API

--- background.js
+++ background.js
@@ -8,13 +8,7 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 });
 
 async function connectAPI(msg){
-  const response = await fetch("http://localhost:5000/api/generate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ selectedText: msg.selectedText, operationChoice: msg.operationChoice  }),
-  });
+  const response = await fetch(`http://localhost:5000/api/generate/${msg.selectedText}&${msg.operationChoice}`);
   if (response.status === 200) {
     const data = await response.json();
     return(data);
